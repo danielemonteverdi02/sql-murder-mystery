3439882648b0243ea376c7db789541afb7ce1d47
Daniele Monteverdi <danielemonteverdi70@gmail.com>
2026-04-28 21:32:05 +0200

    Delete 05_vehicle_check.sql

--- 05_vehicle_check.sql
+++
@@ -1,8 +0,0 @@
-/* STEP 5 — VEHICLE IDENTIFICATION
- * Objective: confirm suspect using license plate clue ("H42W")
- */
-
-SELECT *
-FROM drivers_license dl 
-INNER JOIN person p ON dl.id = p.license_id
-WHERE (p.id = 28819 OR p.id = 67318) AND dl.plate_number LIKE "%H42W%";
