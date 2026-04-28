bcf7045b686e474c42cbb2cc656a099e85d2d8b3
Daniele Monteverdi <danielemonteverdi70@gmail.com>
2026-04-28 21:31:38 +0200

    Delete 03_interviews.sql

--- 03_interviews.sql
+++
@@ -1,9 +0,0 @@
-/* STEP 3 — WITNESS INTERVIEWS
- * Objective: extract statements from identified witnesses
- * Purpose: obtain clues about suspect identity and behavior
- */
-
-SELECT *
-FROM interview i
-INNER JOIN person p ON i.person_id = p.id
-WHERE i.person_id = 14887 OR i.person_id = 16371;
