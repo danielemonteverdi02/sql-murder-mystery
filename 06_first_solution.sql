be4436121dd2589439b88704d288633b5cbb17da
Daniele Monteverdi <danielemonteverdi70@gmail.com>
2026-04-28 21:32:27 +0200

    Delete 06_first_solution.sql

--- 06_first_solution.sql
+++
@@ -1,8 +0,0 @@
-/* STEP 6 — INITIAL SUSPECT IDENTIFIED
- * Result: Jeremy Bowers is identified as the murderer
- */
-
-INSERT INTO solution VALUES (1, "Jeremy Bowers");
-
-SELECT value
-FROM solution s;
